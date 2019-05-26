96d0e8494e062e705dcd6c37599c9d7ab751f0e4
David Souza <david.jsouzaa@gmail.com>
2019-05-26 00:43:25 -0300

    starting project

--- db/myDb.sql
+++ db/myDb.sql
@@ -11,7 +11,7 @@ PRIMARY KEY(id)
 CREATE TABLE products (
 id serial,
 name varchar(60) NOT NULL,
-price decimal(6, 2),
+price numeric(10),
 description varchar(255),
 img_path varchar(255),
 PRIMARY KEY(id)
@@ -28,11 +28,11 @@ PRIMARY KEY(order_id)
 
 INSERT INTO products (name, price, description, img_path)
 VALUES ('T-Shirt Dance', 29, 'Beautiful, and light T-Shirt to practice sports', 'img/store/10.jpg'),
-('Dark T-Shirt', '39', 'Fashion, ideal to casual uses', 'img/store/11.jpg'),
-('Dark T-Shirt', '35', 'Fashion, ideal to casual uses', 'img/store/3.jpg'),
-('Dark T-Shirt', '32', 'Fashion, ideal to casual uses', 'img/store/4.jpg'),
-('Dark T-Shirt', '25', 'Fashion, ideal to casual uses', 'img/store/5.jpg'),
-('Dark T-Shirt', '54', 'Fashion, ideal to casual uses', 'img/store/6.jpg'),
-('Dark T-Shirt', '74', 'Fashion, ideal to casual uses', 'img/store/7.jpg'),
-('Dark T-Shirt', '15', 'Fashion, ideal to casual uses', 'img/store/8.jpg'),
-('Dark T-Shirt', '45', 'Fashion, ideal to casual uses', 'img/store/9.jpg');
+('Dark T-Shirt', 39, 'Fashion, ideal to casual uses', 'img/store/11.jpg'),
+('Dark T-Shirt', 35, 'Fashion, ideal to casual uses', 'img/store/3.jpg'),
+('Dark T-Shirt', 32, 'Fashion, ideal to casual uses', 'img/store/4.jpg'),
+('Dark T-Shirt', 25, 'Fashion, ideal to casual uses', 'img/store/5.jpg'),
+('Dark T-Shirt', 54, 'Fashion, ideal to casual uses', 'img/store/6.jpg'),
+('Dark T-Shirt', 74, 'Fashion, ideal to casual uses', 'img/store/7.jpg'),
+('Dark T-Shirt', 15, 'Fashion, ideal to casual uses', 'img/store/8.jpg'),
+('Dark T-Shirt', 45, 'Fashion, ideal to casual uses', 'img/store/9.jpg');
