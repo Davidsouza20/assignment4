2070fa4916df1a5ab5ea3f205ac014a6bcb0de4b
David Souza <david.jsouzaa@gmail.com>
2019-05-26 00:37:11 -0300

    starting project

--- db/myDb.sql
+++ db/myDb.sql
@@ -28,11 +28,11 @@ PRIMARY KEY(order_id)
 
 INSERT INTO products (name, price, description, img_path)
 VALUES ('T-Shirt Dance', 29, 'Beautiful, and light T-Shirt to practice sports', 'img/store/10.jpg'),
-('Dark T-Shirt', 39, 'Fashion, ideal to casual uses', 'img/store/11.jpg'),
-('Dark T-Shirt', 35, 'Fashion, ideal to casual uses', 'img/store/3.jpg'),
-('Dark T-Shirt', 32, 'Fashion, ideal to casual uses', 'img/store/4.jpg'),
-('Dark T-Shirt', 25, 'Fashion, ideal to casual uses', 'img/store/5.jpg'),
-('Dark T-Shirt', 54, 'Fashion, ideal to casual uses', 'img/store/6.jpg'),
-('Dark T-Shirt', 74, 'Fashion, ideal to casual uses', 'img/store/7.jpg'),
-('Dark T-Shirt', 15, 'Fashion, ideal to casual uses', 'img/store/8.jpg'),
-('Dark T-Shirt', 45, 'Fashion, ideal to casual uses', 'img/store/9.jpg');
+('Dark T-Shirt', '39', 'Fashion, ideal to casual uses', 'img/store/11.jpg'),
+('Dark T-Shirt', '35', 'Fashion, ideal to casual uses', 'img/store/3.jpg'),
+('Dark T-Shirt', '32', 'Fashion, ideal to casual uses', 'img/store/4.jpg'),
+('Dark T-Shirt', '25', 'Fashion, ideal to casual uses', 'img/store/5.jpg'),
+('Dark T-Shirt', '54', 'Fashion, ideal to casual uses', 'img/store/6.jpg'),
+('Dark T-Shirt', '74', 'Fashion, ideal to casual uses', 'img/store/7.jpg'),
+('Dark T-Shirt', '15', 'Fashion, ideal to casual uses', 'img/store/8.jpg'),
+('Dark T-Shirt', '45', 'Fashion, ideal to casual uses', 'img/store/9.jpg');
