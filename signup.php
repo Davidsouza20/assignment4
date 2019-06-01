bc8b1ee8beef21c9a999b442635cd80a7b7b5b3c
David Souza <david.jsouzaa@gmail.com>
2019-06-01 14:35:34 -0300

    update db

--- signup.php
+++ signup.php
@@ -59,14 +59,9 @@
     <title>Sign Up</title>
    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    
-    
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
 
-<link rel="stylesheet" type="text/css" href="signup.css">
+
+    <link rel="stylesheet" type="text/css" href="signup.css">
 </head>
 <body>
     <div class="wrapper">
